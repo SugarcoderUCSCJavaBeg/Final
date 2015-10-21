29da73575a5a594d02fb085bca72a928bba3b5a0
Sugarcoder <mikichea@gmail.com>
2015-10-21 11:58:56 -0700

    try to read file

--- LetterGrader.java
+++ LetterGrader.java
@@ -1,31 +1,50 @@
-/**
- * Created by Sugarcoder on 10/20/2015.
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class LetterGrader {
-
-    // This file does all the work, reads the input data,
-    // calculates the grade averages, and prints the data.
-
-
-    /* Test scores are weighed.
-       4 Quizzes: 40% (10% each)
-       Midterm 1: 20%
-       Midterm 2: 15%
-       Final: 25%
-
-       Need to apply weight for each score in the program to calculate the final score.
-
-        Final Score: quiz1*.10 + quiz2*.10 + quiz3*.10 + quiz4*.10 +
+    /* Total Score (100%): quiz1*.10 + quiz2*.10 + quiz3*.10 + quiz4*.10 +
                     mid1*.20 + mid2*.15 + final*.25
-
-        Determination of grade:
-                    >=90% = A
-                    80-89% = B
-                    70-79% = C
-                    60-69% = D
-                    <=59% = F
+        Determination of grade: >=90% = A, 80-89% = B, 70-79% = C, 60-69% = D, <=59% = F
     */
 
+    private static String fileName = "input_data";
 
+
+    public static void main(String[] args) throws IOException {
+        // Scanner scannerName = new Scanner(new File("input_data.txt"));
+
+
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(fileName)));
+
+
+        // Finds out where the path is: System.out.println(new File("input_data").getAbsoluteFile());
+
+
+//        try {
+//            StringBuilder sb = new StringBuilder();
+//            String line = bufferedReader.readLine();
+//
+//            while (line != null) {
+//                sb.append(line);
+//                sb.append(System.lineSeparator());
+//                line = bufferedReader.readLine();
+//            }
+//            String everything = sb.toString();
+//        } finally {
+//            bufferedReader.close();
+//        }
+//
+//    }
+
+
+//    public static void readScore() {
+//
+//    }
+
+
+    }
 }
--- TestLetterGrader.java
+++ TestLetterGrader.java
@@ -1,13 +1,5 @@
-/**
- * Created by Sugarcoder on 10/20/2015.
- */
 
 public class TestLetterGrader {
-
-
-
-
-
 
 
 }
