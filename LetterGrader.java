c11a1f6f2d570f463fa74d2d04e2dc0307f2c416
Sugarcoder <mikichea@gmail.com>
2015-10-23 22:41:18 -0700

    able to read file

--- LetterGrader.java
+++ LetterGrader.java
@@ -1,4 +1,6 @@
 import java.io.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
     /* Total Score (100%): quiz1*.10 + quiz2*.10 + quiz3*.10 + quiz4*.10 +
@@ -11,24 +13,73 @@ public class LetterGrader {
     // Gets the exact path: System.out.println(new File("input.txt").getAbsolutePath());
 
 
-    public static int readScore() {
-        try {
-            Scanner diskScanner = new Scanner(new File("input.txt"));
-            while (diskScanner.hasNextLine()) {
-                System.out.println(diskScanner.nextLine());
-            }
-            diskScanner.close();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        public int readScore () {
+            try {
+                Scanner diskScanner = new Scanner(new File("input.txt"));
+                while (diskScanner.hasNextLine()) {
+                    System.out.println(diskScanner.nextLine());
+                }
+                diskScanner.close();
+                System.out.println("****************************\n");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return 0;
         }
+
+
+
+
+    public long calcLetterGrade() throws NumberFormatException, IOException {
+
+        Scanner calcScanner = new Scanner(new File("input.txt"));
+        ArrayList<Double> averages = new ArrayList<>();
+
+        double max = 0, min = 100;
+
+        while(calcScanner.hasNextLine()) {
+
+            String line = calcScanner.nextLine();
+            String[] score = line.split(",");
+
+
+            double[] points = new double[score.length - 1];
+
+            for (int i = 1; i < score.length; i++) {
+                points[i - 1] = Double.parseDouble(score[i]);
+            }
+
+            double sumNum = 0.0;
+
+            for (double i : points) {
+                sumNum += i;
+                min = Math.min(min, i);
+                max = Math.max(max, i);
+            }
+
+            double avg = sumNum / 10;
+
+
+            System.out.println(avg);
+        }
+
+        calcScanner.close();
+        double avgSum = .0;
+
+        for (double avg : averages) {
+            avgSum += avg;
+        }
+        System.out.println("Maximum: " + max + "");
+        System.out.println("Minimum: " + min + "");
+        System.out.println("Average: " + avgSum / averages.size() + " ");
+
+
+
+
         return 0;
     }
-
-
-    
 }
-
 
 
 
