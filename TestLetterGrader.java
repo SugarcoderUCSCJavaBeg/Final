1890123e0ba7008b1e5b6b71bd118c2c9fe613f5
Sugarcoder <mikichea@gmail.com>
2015-11-05 01:57:26 -0800

    finished calculating averages

--- TestLetterGrader.java
+++ TestLetterGrader.java
@@ -1,22 +1,33 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TestLetterGrader {
 
     public static void main(String[] args) throws IOException {
+
+        boolean quit = false;
         LetterGrader letterGrader = new LetterGrader();
-        System.out.println("This is all of the scores from the file.");
+        System.out.println("These are all the scores from the file.");
         System.out.println("\n");
         letterGrader.readScore();
         System.out.println("******************\n");
 
-        // System.out.println("Displaying average scores, minimum scores and maximum scores.");
-        // System.out.println("Q1    Q2    Q3    Q4    M1    M2    F");
-        // System.out.println("..... Press enter to continue");
 
-        System.out.println("This is all the averages of the students.");
-        
+        System.out.println("Here are the class average, minimum, and maximum scores.");
+        System.out.println("         Q1            Q2          Q3            Q4           M1           M2           F");
+        System.out.printf("Average: " + "%f%12f%12f%12f%12f%12f%12f\n", displayAverages.Q1avg(), displayAverages.Q2avg(), displayAverages.Q3avg(), displayAverages.Q4avg(), displayAverages.mid1avg(), displayAverages.mid2avg(), displayAverages.final1avg());
+        System.out.printf("Minimum: " + "%d%12d%12d%12d%12d%12d%12d\n", displayAverages.Q1min(), displayAverages.Q2min(), displayAverages.Q3min(), displayAverages.Q4min(), displayAverages.mid1min(), displayAverages.mid2min(), displayAverages.final1min());
+        System.out.printf("Maximum: " + "%d%12d%12d%12d%12d%12d%12d\n", displayAverages.Q1max(), displayAverages.Q2max(), displayAverages.Q3max(), displayAverages.Q4max(), displayAverages.mid1max(), displayAverages.mid2max(), displayAverages.final1max() );
+        System.out.println(" ");
 
-        // letterGrader.displayAssnAverages();      // Displays the average in the console (don't write to the fle)
+        while(!quit) {
+            System.out.println("..... Press Enter to continue");
+            Scanner keyboard = new Scanner(System.in);
+            keyboard.nextLine();
+            System.exit(0);
+        }
+
+
 
 
         // System.out.println("******************\n");
