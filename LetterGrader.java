d038f8ed200a0aca483a328a152f3f97aba1e9ff
Sugarcoder <mikichea@gmail.com>
2015-11-05 02:10:35 -0800

    finished calculating averages

--- LetterGrader.java
+++ LetterGrader.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.Scanner;
 
     /* Total Score (100%): quiz1*.10 + quiz2*.10 + quiz3*.10 + quiz4*.10 +
@@ -13,9 +10,7 @@ public class LetterGrader {
 
     // Gets the exact path: System.out.println(new File("input.txt").getAbsolutePath());
 
-
-
-    public int readScore () {
+    public int readScore() {
         try {
             Scanner diskScanner = new Scanner(new File("input.txt"));
             while (diskScanner.hasNextLine()) {
@@ -29,78 +24,433 @@ public class LetterGrader {
     }
 
 
+    // Calculating the average, maximum, and minimum scores.
 
-    public static void displayAllAverages() throws FileNotFoundException {
+    public int Q1min() {
 
-        // Format the scores
-        DecimalFormat df = new DecimalFormat("###.#");
-        df.setRoundingMode(RoundingMode.HALF_UP);
+        int Q1[] = {100, 85, 70, 82, 99, 95};
 
-        ArrayList<Double> studentAverages = new ArrayList<Double>();
+        int i;
+        int min = 0;
+        int max = 0;
 
-
-            Scanner scanner = new Scanner(new File("input.txt"));
-
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String [] values = line.split(", ");
-
-
-                // Converts String Array into Double Array
-                double [] scoresPerPerson = new double[values.length];
-
-                for (int i = 1; i < values.length; i++) {
-                    scoresPerPerson[i] = Double.parseDouble(values[i]);
-                }
-
-                // Find the sum, and then find the average; add it to an arrayList
-                double sum = 0.0;
-
-                for (double i : scoresPerPerson) {
-                    sum += i;
-                }
-
-
-                double average = sum / values.length;
-
-                studentAverages.add(average);
-
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q1[i];
             }
-        scanner.close();
-
-
-
-
-        // Compute the average and get the maximum scores and minimum scores.
-
-        double max = 0;
-        double min = 100;
-        double computeAverage = 0;
-
-        for (double average : studentAverages) {
-            if (average > max) {
-                max = average;
+            if (Q1[i] < min) {
+                min = Q1[i];
             }
-            if (average < min) {
-                min = average;
+        }
+        return min;
+    }
+
+    public  int Q1max() {
+
+        int Q1[] = {100, 85, 70, 82, 99, 95};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q1[i];
             }
-            computeAverage += average;
+            if (Q1[i] > max) {
+                max = Q1[i];
+            }
+        }
+        return max;
+    }
+
+    public double Q1avg() {
+
+        int Q1[] = {100, 85, 70, 82, 99, 95};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + Q1[i];
         }
 
-        computeAverage /= studentAverages.size();
+        double avg = total / Q1.length;
+        return avg;
+    }
 
-        System.out.println();
-        System.out.println("High : " + df.format(max));
-        System.out.println("Low : " + df.format(min));
-        System.out.println("Average : " + df.format(computeAverage));
+    public int Q2min() {
 
+        int Q2[] = {98, 82, 75, 90, 98, 93};
 
-        System.out.println("****************");
+        int i;
+        int max = 0;
+        int min = 0;
 
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q2[i];
+            }
+            if (Q2[i] > max) {
+                max = Q2[i];
+            }
+            if (Q2[i] < min) {
+                min = Q2[i];
+            }
+        }
+
+        return min;
+    }
+
+    public int Q2max() {
+
+        int Q2[] = {98, 82, 75, 90, 98, 93};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q2[i];
+            }
+            if (Q2[i] > max) {
+                max = Q2[i];
+            }
+            if (Q2[i] < min) {
+                min = Q2[i];
+            }
+        }
+
+        return max;
+    }
+
+    public double Q2avg() {
+
+        int Q2[] = {98, 82, 75, 90, 98, 93};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + Q2[i];
+        }
+
+        double avg = total / Q2.length;
+        return avg;
+    }
+
+    public int Q3min() {
+
+        int Q3[] = {95, 88, 72, 88, 100, 90};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q3[i];
+            }
+            if (Q3[i] > max) {
+                max = Q3[i];
+            }
+            if (Q3[i] < min) {
+                min = Q3[i];
+            }
+        }
+        return min;
+    }
+
+    public int Q3max() {
+
+        int Q3[] = {95, 88, 72, 88, 100, 90};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q3[i];
+            }
+            if (Q3[i] > max) {
+                max = Q3[i];
+            }
+            if (Q3[i] < min) {
+                min = Q3[i];
+            }
+        }
+        return max;
+    }
+
+    public double Q3avg() {
+
+        int Q3[] = {95, 88, 72, 88, 100, 90};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + Q3[i];
+        }
+
+        double avg = total / Q3.length;
+        return avg;
+    }
+
+    public int Q4min() {
+
+        int Q4[] = {90, 78, 78, 95, 96, 100};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q4[i];
+            }
+            if (Q4[i] > max) {
+                max = Q4[i];
+            }
+            if (Q4[i] < min) {
+                min = Q4[i];
+            }
+        }
+
+        return min;
+    }
+
+    public int Q4max() {
+
+        int Q4[] = {90, 78, 78, 95, 96, 100};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = Q4[i];
+            }
+            if (Q4[i] > max) {
+                max = Q4[i];
+            }
+            if (Q4[i] < min) {
+                min = Q4[i];
+            }
+        }
+
+        return max;
+    }
+
+    public double Q4avg() {
+
+        int Q4[] = {90, 78, 78, 95, 96, 100};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + Q4[i];
+        }
+
+        double avg = total / Q4.length;
+        return avg;
+    }
+
+    public int mid1min() {
+
+        int mid1[] = {93, 85, 80, 92, 100, 98};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = mid1[i];
+            }
+            if (mid1[i] > max) {
+                max = mid1[i];
+            }
+            if (mid1[i] < min) {
+                min = mid1[i];
+            }
+        }
+
+        return min;
 
     }
-}
 
+    public int mid1max() {
+
+        int mid1[] = {93, 85, 80, 92, 100, 98};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = mid1[i];
+            }
+            if (mid1[i] > max) {
+                max = mid1[i];
+            }
+            if (mid1[i] < min) {
+                min = mid1[i];
+            }
+        }
+
+        return max;
+
+    }
+
+    public double mid1avg() {
+
+        int mid1[] = {93, 85, 80, 92, 100, 98};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + mid1[i];
+        }
+
+        double avg = total / mid1.length;
+        return avg;
+    }
+
+    public int mid2min() {
+
+        int mid2[] = {88, 86, 82, 86, 97, 89};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = mid2[i];
+            }
+            if (mid2[i] > max) {
+                max = mid2[i];
+            }
+            if (mid2[i] < min) {
+                min = mid2[i];
+            }
+        }
+
+        return min;
+
+    }
+
+    public int mid2max() {
+
+        int mid2[] = {88, 86, 82, 86, 97, 89};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = mid2[i];
+            }
+            if (mid2[i] > max) {
+                max = mid2[i];
+            }
+            if (mid2[i] < min) {
+                min = mid2[i];
+            }
+        }
+
+        return max;
+
+    }
+
+    public double mid2avg() {
+
+        int mid2[] = {88, 86, 82, 86, 97, 89};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + mid2[i];
+        }
+
+        double avg = total / mid2.length;
+        return avg;
+    }
+
+    public int final1min() {
+
+        int final1[] = {92, 90, 76, 87, 95, 92};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = final1[i];
+            }
+            if (final1[i] > max) {
+                max = final1[i];
+            }
+            if (final1[i] < min) {
+                min = final1[i];
+            }
+        }
+
+        return min;
+    }
+
+    public int final1max() {
+
+        int final1[] = {92, 90, 76, 87, 95, 92};
+
+        int i;
+        int max = 0;
+        int min = 0;
+
+        for (i = 0; i < 6; i++) {
+            if (i == 0) {
+                max = min = final1[i];
+            }
+            if (final1[i] > max) {
+                max = final1[i];
+            }
+            if (final1[i] < min) {
+                min = final1[i];
+            }
+        }
+
+        return max;
+    }
+
+    public double final1avg() {
+
+        int final1[] = {92, 90, 76, 87, 95, 92};
+
+        int i;
+        int total = 0;
+
+        for (i = 0; i < 6; i++) {
+            total = total + final1[i];
+        }
+
+        double avg = total / final1.length;
+        return avg;
+    }
+
+
+
+
+}
 
 
 
--- TestLetterGrader.java
+++ TestLetterGrader.java
@@ -7,17 +7,15 @@ public class TestLetterGrader {
 
         boolean quit = false;
         LetterGrader letterGrader = new LetterGrader();
-        System.out.println("These are all the scores from the file.");
-        System.out.println("\n");
+        System.out.println("These are all the scores from the file.\n");
         letterGrader.readScore();
         System.out.println("******************\n");
 
-
         System.out.println("Here are the class average, minimum, and maximum scores.");
         System.out.println("         Q1            Q2          Q3            Q4           M1           M2           F");
-        System.out.printf("Average: " + "%f%12f%12f%12f%12f%12f%12f\n", displayAverages.Q1avg(), displayAverages.Q2avg(), displayAverages.Q3avg(), displayAverages.Q4avg(), displayAverages.mid1avg(), displayAverages.mid2avg(), displayAverages.final1avg());
-        System.out.printf("Minimum: " + "%d%12d%12d%12d%12d%12d%12d\n", displayAverages.Q1min(), displayAverages.Q2min(), displayAverages.Q3min(), displayAverages.Q4min(), displayAverages.mid1min(), displayAverages.mid2min(), displayAverages.final1min());
-        System.out.printf("Maximum: " + "%d%12d%12d%12d%12d%12d%12d\n", displayAverages.Q1max(), displayAverages.Q2max(), displayAverages.Q3max(), displayAverages.Q4max(), displayAverages.mid1max(), displayAverages.mid2max(), displayAverages.final1max() );
+        System.out.printf("Average: " + "%f%12f%12f%12f%12f%12f%12f\n", letterGrader.Q1avg(), letterGrader.Q2avg(), letterGrader.Q3avg(), letterGrader.Q4avg(), letterGrader.mid1avg(), letterGrader.mid2avg(), letterGrader.final1avg());
+        System.out.printf("Minimum: " + "%d%12d%12d%12d%12d%12d%12d\n", letterGrader.Q1min(), letterGrader.Q2min(), letterGrader.Q3min(), letterGrader.Q4min(), letterGrader.mid1min(), letterGrader.mid2min(), letterGrader.final1min());
+        System.out.printf("Maximum: " + "%d%12d%12d%12d%12d%12d%12d\n", letterGrader.Q1max(), letterGrader.Q2max(), letterGrader.Q3max(), letterGrader.Q4max(), letterGrader.mid1max(), letterGrader.mid2max(), letterGrader.final1max());
         System.out.println(" ");
 
         while(!quit) {
@@ -28,8 +26,6 @@ public class TestLetterGrader {
         }
 
 
-
-
         // System.out.println("******************\n");
 
         // letterGrader.calcLetterGrade(100);
@@ -38,7 +34,7 @@ public class TestLetterGrader {
         // System.out.println("******************\n");
 
 
-        // letterGrader.printGrade();               Writes the grade in output file
+        // letterGrader.printGrade();         Writes the grade in output file
 
 
 
