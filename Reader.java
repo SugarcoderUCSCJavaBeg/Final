77f762ec5b9996e031723fe7a95cf22ed6498d18
Sugarcoder <mikichea@gmail.com>
2015-11-23 00:00:56 -0800

    updates

--- Reader.java
+++ Reader.java
@@ -1,11 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
 
-    /* Total Score (100%): quiz1*.10 + quiz2*.10 + quiz3*.10 + quiz4*.10 +
-                    mid1*.20 + mid2*.15 + final*.25
-        Determination of grade: >=90% = A, 80-89% = B, 70-79% = C, 60-69% = D, <=59% = F
-    */
-
 public class Reader {
 
     // Gets the exact path: System.out.println(new File("input.txt").getAbsolutePath());
